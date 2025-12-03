b8c750bd22881c5f7d76c0492e6bec722428a0e3
Ben Frearson <57793137+bfrearson@users.noreply.github.com>
2025-12-03 08:27:43 +0000

    Day 3, part 1

---
+++ Day 3/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// var input = File.ReadAllText("Input.txt");
+// Console.WriteLine(Part1(input.Split("\n")));
+// Isaac's Algorithm
+long Part1(string[] banks)
+{
+    var joltages = new List<long>();
+    
+    foreach(var bank in banks)
+    {
+        int firstDigitPosition = 0;
+        int secondDigitPosition = 0;
+        
+        for (var i = 9; i >= 0; i--)
+        {
+            // Console.WriteLine($"Scanning for number {i} in bank {bank}");
+            
+            if (bank[..^1].Contains(i.ToString()))
+            {
+                firstDigitPosition = bank[..^1].IndexOf(i.ToString());
+                break;
+            }
+        }
+        
+        for (var i = 9; i >= 0; i--)
+        {
+            if (bank[(firstDigitPosition + 1)..].Contains(i.ToString()))
+            {
+                secondDigitPosition = bank[(firstDigitPosition + 1)..].IndexOf(i.ToString()) + firstDigitPosition + 1;
+                break;
+            }
+        }
+
+        
+        var maximumJoltage = bank[firstDigitPosition].ToString() + bank[secondDigitPosition].ToString();
+        Console.WriteLine($"Maximum joltage for bank {bank} is {maximumJoltage}");
+        joltages.Add(long.Parse(maximumJoltage));
+    }
+    return joltages.Sum();
+}
