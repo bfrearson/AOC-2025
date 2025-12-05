a53ce39563d636b70990209d23b0801a91e2167a
Ben Frearson <57793137+bfrearson@users.noreply.github.com>
2025-12-05 06:31:59 +0000

    Day 5, part 1

---
+++ Day5/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var input = File.ReadAllText("Input.txt");
+Console.WriteLine(Part1(input.Split("\n\n")));
+
+// var input = File.ReadAllText("Input.txt");
+// Console.WriteLine(Part2(input.Split("\n")));
+
+long Part1(string[] inventory)
+{
+    var ingredientIds = inventory[0].Split("\n");
+    var ingredients = inventory[1].Split("\n").Select(x => long.Parse(x)).ToList();
+
+    var freshIngredients = new List<long>();
+    foreach (var ingredient in ingredients)
+    {
+        foreach (var idRange in ingredientIds)
+        {
+            var range= idRange.Split("-").Select(x => long.Parse(x)).ToArray();
+            if (ingredient >= range[0] && ingredient <= range[1])
+            {
+                freshIngredients.Add(ingredient);
+                break;
+            }
+        }
+    }
+    return freshIngredients.Count;
+}
