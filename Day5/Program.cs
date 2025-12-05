d3caa137ef83cab5941caac1e0ded21dea461ebd
Ben Frearson <57793137+bfrearson@users.noreply.github.com>
2025-12-05 23:00:35 +0000

    Day 5, part 2 done! :)

--- Day5/Program.cs
+++ Day5/Program.cs
@@ -27,66 +27,36 @@ long Part1(string[] inventory)
 
 long Part2(string[] inventory)
 {
-    var ingredientIds = inventory[0].Split("\n");
-    var compressedIds = new List<(long,long)>();
-    
-    foreach (var idRange in ingredientIds)
+    List<(long, long)> ingredientIds = inventory[0].Split("\n").Select(x =>
+        {
+            var range = x.Split("-").Select(x => long.Parse(x)).ToArray();
+            return (range[0], range[1]);
+        })
+        .ToList();
+
+    ingredientIds.Sort(delegate((long, long) a, (long, long) b)
     {
-        // Console.WriteLine($"Searching for {idRange}");
-        var range= idRange.Split("-").Select(x => long.Parse(x)).ToArray();
-        (long min, long max) = (range[0], range[1]);
-        
-        var lowerOverlapIndex = 0;
-        while (lowerOverlapIndex < compressedIds.Count && min > compressedIds[lowerOverlapIndex].Item2) 
-        {
-            lowerOverlapIndex++;
-        }
-
-        if (lowerOverlapIndex >= compressedIds.Count)
-        {
-            compressedIds.Add((min, max));
-            continue;
-        }
-        
-        if (max < compressedIds[lowerOverlapIndex].Item1)
-        {
-            compressedIds.Insert(lowerOverlapIndex, (min, max));
-            continue;
-        }
-
-        var upperOverlapIndex = lowerOverlapIndex;
-        while (upperOverlapIndex < compressedIds.Count && max > compressedIds[upperOverlapIndex].Item1)
-        {
-            upperOverlapIndex++;
-        }
-
-        upperOverlapIndex = int.Min(upperOverlapIndex, compressedIds.Count - 1);
-        min = long.Min(min, compressedIds[lowerOverlapIndex].Item1);
-        max = long.Max(max, compressedIds[upperOverlapIndex].Item2);
-        
-        compressedIds.Insert(lowerOverlapIndex, (min, max));
-        // Console.WriteLine($"Inserted{compressedIds[lowerOverlapIndex]}");
-        Console.WriteLine($"Removing {upperOverlapIndex - lowerOverlapIndex + 1} entries");
-        compressedIds.RemoveRange(lowerOverlapIndex + 1,  upperOverlapIndex - lowerOverlapIndex + 1);
-        
-        if (compressedIds.Count == 1)
-            Console.WriteLine($"New count: {compressedIds.Count}");
+        if (a.Item1 < b.Item1)
+            return -1;
+        else return 1;
+    });
+    
+    var compressedIds = new List<(long, long)>();
+    compressedIds.Add(ingredientIds[0]);
+    foreach (var ingredientRange in ingredientIds)
+    {
+        var lastRange = compressedIds.Last();
+            if (lastRange.Item2 < ingredientRange.Item1)
+            {
+                compressedIds.Add(ingredientRange);
+            }
+            else
+            {
+                var min = long.Min(ingredientRange.Item1, compressedIds[^1].Item1);
+                var max = long.Max(ingredientRange.Item2, compressedIds[^1].Item2);
+                compressedIds[^1] = (min, max);
+            }
     }
-
-    // foreach (var idRange in ingredientIds)
-    // {
-    //     var range = idRange.Split("-").Select(x => long.Parse(x)).ToArray();
-    //     (long min, long max) = (range[0], range[1]);
-    //     var count = 0;
-    //     foreach (var id in compressedIds)
-    //     {
-    //         if (min >= id.Item1 && min <= id.Item2 || max <= id.Item2 && max >= id.Item1)
-    //         {
-    //             count++;
-    //         }
-    //     }
-    //     Console.WriteLine($"{count}");
-    // }
 
     return compressedIds.Select(x => x.Item2 - x.Item1 + 1).Sum();
 }
