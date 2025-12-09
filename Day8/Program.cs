6175e3e7c7a5fd3a4c2f60ac9901eaac61b28d10
Ben Frearson <57793137+bfrearson@users.noreply.github.com>
2025-12-09 06:19:49 +0000

    Day 8, Part 2

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -1,4 +1,4 @@
-﻿var input = File.ReadAllText("Sample.txt");
+﻿var input = File.ReadAllText("Input.txt");
 // Console.WriteLine(Part1(input.Split("\n")));
 Console.WriteLine(Part2(input.Split("\n")));
 
@@ -8,7 +8,7 @@ long Part1(string[] lines)
     // 1. Store all distances between nodes
     for (var i = 0; i < lines.Length; i++)
     {
-        for (var j = i+1; j < lines.Length; j++)
+        for (var j = i + 1; j < lines.Length; j++)
         {
             var box1 = GetBox(lines[i]);
             var box2 = GetBox(lines[j]);
@@ -16,15 +16,15 @@ long Part1(string[] lines)
             distances.Add((distance, (lines[i], lines[j])));
         }
     }
-    
-    distances.Sort((a, b) =>a.distance.CompareTo(b.distance));
+
+    distances.Sort((a, b) => a.distance.CompareTo(b.distance));
 
     var maxConnections = int.Min(1000, distances.Count() - 1);
     distances = distances[..(maxConnections)];
 
     List<string> nodes = new List<string>(lines);
     var connections = new List<List<string>>();
-    
+
     foreach (var link in distances)
     {
         var isBox1Present = !nodes.Contains(link.boxes.Item1);
@@ -33,25 +33,25 @@ long Part1(string[] lines)
         switch (isBox1Present, isBox2Present)
         {
             case (true, true):
-             {
-                 int box1ListIndex = 0;
-                 int box2ListIndex = 0;
-                 for (var i = 0; i < connections.Count; i++)
-                 {
-                     var circuit = connections[i];
-                     if (circuit.Contains(link.boxes.Item1))
-                         box1ListIndex = i;
-                     if (circuit.Contains(link.boxes.Item2))
-                         box2ListIndex = i;
-                 }
-                 
-                 if (box1ListIndex == box2ListIndex)
-                     break;
-                 var combinedList = connections[box1ListIndex].Concat(connections[box2ListIndex]).ToList();
-                 connections[box1ListIndex] = combinedList;
-                 connections.Remove(connections[box2ListIndex]);
-                 break;
-             }
+            {
+                int box1ListIndex = 0;
+                int box2ListIndex = 0;
+                for (var i = 0; i < connections.Count; i++)
+                {
+                    var circuit = connections[i];
+                    if (circuit.Contains(link.boxes.Item1))
+                        box1ListIndex = i;
+                    if (circuit.Contains(link.boxes.Item2))
+                        box2ListIndex = i;
+                }
+
+                if (box1ListIndex == box2ListIndex)
+                    break;
+                var combinedList = connections[box1ListIndex].Concat(connections[box2ListIndex]).ToList();
+                connections[box1ListIndex] = combinedList;
+                connections.Remove(connections[box2ListIndex]);
+                break;
+            }
             case (false, true):
             case (true, false):
             {
@@ -71,7 +71,7 @@ long Part1(string[] lines)
             }
             case (false, false):
             {
-                var list = new List<string>{link.boxes.Item1, link.boxes.Item2};
+                var list = new List<string> { link.boxes.Item1, link.boxes.Item2 };
                 connections.Add(list);
                 nodes.Remove(link.boxes.Item1);
                 nodes.Remove(link.boxes.Item2);
@@ -82,6 +82,89 @@ long Part1(string[] lines)
 
     connections.Sort((a, b) => b.Count.CompareTo(a.Count));
     return connections[..3].Aggregate(1L, (acc, list) => acc *= list.Count());
+}
+
+long Part2(string[] lines)
+{
+    var distances = new List<(double distance, (string, string) boxes)>();
+    for (var i = 0; i < lines.Length; i++)
+    {
+        for (var j = i + 1; j < lines.Length; j++)
+        {
+            var box1 = GetBox(lines[i]);
+            var box2 = GetBox(lines[j]);
+            var distance = GetDistance(box1, box2);
+            distances.Add((distance, (lines[i], lines[j])));
+        }
+    }
+
+    distances.Sort((a, b) => a.distance.CompareTo(b.distance));
+
+    List<string> nodes = new List<string>(lines);
+    var connections = new List<List<string>>();
+    
+        foreach (var link in distances)
+        {
+            var isBox1Present = !nodes.Contains(link.boxes.Item1);
+            var isBox2Present = !nodes.Contains(link.boxes.Item2);
+            
+            switch (isBox1Present, isBox2Present)
+            {
+                case (true, true):
+                {
+                    int box1ListIndex = 0;
+                    int box2ListIndex = 0;
+                    for (var i = 0; i < connections.Count; i++)
+                    {
+                        var circuit = connections[i];
+                        if (circuit.Contains(link.boxes.Item1))
+                            box1ListIndex = i;
+                        if (circuit.Contains(link.boxes.Item2))
+                            box2ListIndex = i;
+                    }
+
+                    if (box1ListIndex == box2ListIndex)
+                        break;
+                    var combinedList = connections[box1ListIndex].Concat(connections[box2ListIndex]).ToList();
+                    connections[box1ListIndex] = combinedList;
+                    connections.Remove(connections[box2ListIndex]);
+                    break;
+                }
+                case (false, true):
+                case (true, false):
+                {
+                    var matchedBox = isBox1Present ? link.boxes.Item1 : link.boxes.Item2;
+                    var unmatchedBox = isBox2Present ? link.boxes.Item1 : link.boxes.Item2;
+                    {
+                        foreach (var circuit in connections)
+                        {
+                            if (circuit.Contains(matchedBox))
+                            {
+                                circuit.Add(unmatchedBox);
+                                nodes.Remove(unmatchedBox);
+                            }
+                        }
+                    }
+                    break;
+                }
+                case (false, false):
+                {
+                    var list = new List<string> { link.boxes.Item1, link.boxes.Item2 };
+                    connections.Add(list);
+                    nodes.Remove(link.boxes.Item1);
+                    nodes.Remove(link.boxes.Item2);
+                    break;
+                }
+            }
+
+            if (nodes.Count == 0)
+            {
+                var (node1, node2) = (GetBox(link.boxes.Item1),  GetBox(link.boxes.Item2));
+                return node1.x * node2.x;
+            }
+        }
+
+        return 0;
 }
 
 (int x, int y, int z) GetBox(string line)
@@ -95,6 +178,6 @@ double GetDistance((int x, int y, int z) box1, (int x, int y, int z) box2)
     double xDistance = box1.x - box2.x;
     double yDistance = box1.y - box2.y;
     double zDistance = box1.z - box2.z;
-    
+
     return Math.Pow((xDistance * xDistance) + (yDistance * yDistance) + (zDistance * zDistance), 0.5);
 }
