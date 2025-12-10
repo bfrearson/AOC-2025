ab8e345b0a80ec79ab7684176accf1e4a1f03029
Ben Frearson <ben.frearson@engineius.co.uk>
2025-12-10 08:21:09 +0000

    Day 10, Part 1 wip

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -6,9 +6,19 @@ Console.WriteLine(Part1(input.Split("\n")));
 
 int Part1(string[] input)
 {
+    
+    // Sample passes, but Input doesn't. 
+    // Re-reading the challenge, you can press buttons multiple times.
     var machines = ParseMachines(input);
+    var sum = 0;
+    foreach (var machine in machines)
+    {
+        var fewestPresses = FindFewestPresses(machine);
+        Console.WriteLine($"{fewestPresses}");
+        sum += fewestPresses;
+    }
 
-    return 0;
+    return sum;
 }
 
 Machine[] ParseMachines(string[] input)
@@ -24,10 +34,42 @@ Machine[] ParseMachines(string[] input)
     return machines.ToArray();
 }
 
+
+int FindFewestPresses(Machine machine)
+{
+    var n = machine.buttons.Count;
+    for (var r = 1; r <= n; r++)
+    {
+        for (var i = 0; i <= n - r; i++)
+        {
+            var range = machine.buttons.GetRange(i, r);
+            var lightState = machine.lights.Clone() as bool[];
+
+            foreach (var button in range)
+            {
+                foreach (var index in button)
+                {
+                    var lightIndex = index;
+                    lightState[lightIndex] = lightState[lightIndex] ? false : true;
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", lightState));
+            
+            if (lightState.All(x => x == false))
+            {
+                return r;
+            }
+        }
+    }
+
+    return n;
+}
+
 class Machine
 {
-    private bool[] lights;
-    private List<List<int>> buttons;
+    public bool[] lights;
+    public List<List<int>> buttons;
 
     public Machine(bool[] lights, List<List<int>> buttons)
     {
